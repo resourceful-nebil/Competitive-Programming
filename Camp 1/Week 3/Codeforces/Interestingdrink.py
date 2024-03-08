5b02d139e48da09ab3de4a814824267e9c987660
Nebiyou Elias <122722078+resourceful-nebil@users.noreply.github.com>
2024-03-08 18:49:18 +0300

    Add solution for Interestingdrink

---
+++ Camp 1/Week 3/Codeforces/Interestingdrink.py
@@ -0,0 +1,20 @@
+import bisect
+import sys
+
+def input():
+  return sys.stdin.readline().strip()
+
+t = int(input())
+
+arr = list(map(int,input().split()))
+
+n = int(input())
+
+ans = []
+for i in range(n):
+  ans.append(int(input()))
+
+arr.sort()
+
+for i in range(n):
+  print(bisect.bisect_right(arr,ans[i]))
