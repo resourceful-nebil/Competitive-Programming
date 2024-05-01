fb27a9b461df30abdb66493557e9ce5885c5dc1f
Nebiyou Elias <122722078+resourceful-nebil@users.noreply.github.com>
2024-05-01 09:46:14 +0300

    Time: 36 ms (64.68%) | Memory: 16.4 MB (95.74%) - LeetSync

---
+++ Phase2/67-add-binary/add-binary.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a = int(a,2)
+        b = int(b,2)
+
+        c = a + b
+        c = bin(c)
+        # print(c)
+        return c[2:]
