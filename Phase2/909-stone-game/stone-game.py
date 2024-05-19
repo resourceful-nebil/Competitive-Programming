ba406158c6228a26524b1cdd9bb167d95a3a15c2
Nebiyou Elias <122722078+resourceful-nebil@users.noreply.github.com>
2024-05-19 18:41:53 +0300

    Time: 530 ms (5.02%) | Memory: 118.4 MB (23.26%) - LeetSync

---
+++ Phase2/909-stone-game/stone-game.py
@@ -0,0 +1,21 @@
+class Solution:
+    def stoneGame(self, piles: List[int]) -> bool:
+        memo = {}
+
+        def dp(l,r):
+            if (l,r) in memo:
+                return memo[(l,r)]
+            if l > r:
+                return 0
+            
+            even = True if (r - 1) % 2 else 0
+            left = piles[l] if even else 0
+            right = piles[r] if even else 0
+
+            val = -float('inf')
+            val = max(dp(l,r-1) + right, dp(l + 1,r) + left)
+
+            memo[(l,r)] = val
+            return val
+        
+        return dp(0, len(piles) - 1)
