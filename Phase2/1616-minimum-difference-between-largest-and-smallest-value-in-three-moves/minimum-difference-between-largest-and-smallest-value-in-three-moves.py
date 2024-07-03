48e4e99cff55fcb596c073949fdf7179c60964bf
Nebiyou Elias <122722078+resourceful-nebil@users.noreply.github.com>
2024-07-03 11:16:55 +0300

    Time: 256 ms (91.98%) | Memory: 27.5 MB (39.86%) - LeetSync

--- Phase2/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ Phase2/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -1,13 +1,16 @@
 class Solution:
     def minDifference(self, nums: List[int]) -> int:
-        if len(nums) <= 3:
+        if len(nums) <= 4:
             return 0
-        nums.sort()
         res = float('inf')
 
-        for l in range(4):
-            # for shifiting the window that comes 4 times only 
-            r = len(nums) - 4 + l 
-            res = min(res,nums[r] - nums[l])
-        
+        min_four = (nsmallest(4,nums))
+        max_four = sorted(nlargest(4,nums))
+        # print(min_four)
+        # print(max_four)
+
+        for i in range(4):
+            res = min(res,max_four[i] - min_four[i])
+
+
         return res
