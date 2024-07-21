91f89568eb83843fc7203edf3df5a4e86849a1d1
Nebiyou Elias <122722078+resourceful-nebil@users.noreply.github.com>
2024-07-21 13:14:31 +0300

    Time: 2055 ms (12.82%) | Memory: 17.7 MB (13.77%) - LeetSync

--- Phase2/2044-number-of-wonderful-substrings/number-of-wonderful-substrings.py
+++ Phase2/2044-number-of-wonderful-substrings/number-of-wonderful-substrings.py
@@ -1,16 +1,16 @@
 class Solution:
-  def wonderfulSubstrings(self, word: str) -> int:
-    ans = 0
-    prefix = 0  # the binary prefix
-    count = [0] * 1024  # the binary prefix count
-    count[0] = 1  # the empty string ""
+    def wonderfulSubstrings(self, word: str) -> int:
+        mask_counter = Counter({0:1})
+        cnt = 0
+        st = 0
 
-    for c in word:
-      prefix ^= 1 << ord(c) - ord('a')
-      # All the letters occur even number of times.
-      ans += count[prefix]
-      # `c` occurs odd number of times.
-      ans += sum(count[prefix ^ 1 << i] for i in range(10))
-      count[prefix] += 1
+        for c in word:
+            st ^= 1 << (ord(c) - ord('a'))
+            cnt += mask_counter[st]
 
-    return ans
+            for i in range(10):
+                cnt += mask_counter[st  ^ (1 << i)]
+            
+            mask_counter[st] += 1
+        
+        return cnt 
