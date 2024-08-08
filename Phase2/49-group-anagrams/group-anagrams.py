8df4732822bd8445fb3943022fde3efe6c2281e9
Nebiyou Elias <122722078+resourceful-nebil@users.noreply.github.com>
2024-08-08 07:34:08 +0300

    Time: 86 ms (74.41%) | Memory: 22.3 MB (9.12%) - LeetSync

--- Phase2/49-group-anagrams/group-anagrams.py
+++ Phase2/49-group-anagrams/group-anagrams.py
@@ -3,11 +3,11 @@ class Solution:
         res = defaultdict(list)
 
         for s in strs:
-            count = [0] * 26
+            cnt = [0] * 26
 
             for c in s:
-                count[ord(c) - ord("a")] += 1
+                cnt[ord(c) - ord('a')] += 1
             
-            res[tuple(count)].append(s)
+            res[tuple(cnt)].append(s)
         
         return res.values()
