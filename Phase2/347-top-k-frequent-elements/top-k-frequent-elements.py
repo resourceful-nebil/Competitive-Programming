5e282de894bd86b6cd4a00fbde777c722d2616d2
Nebiyou Elias <122722078+resourceful-nebil@users.noreply.github.com>
2024-08-09 21:23:49 +0300

    Time: 102 ms (10.79%) | Memory: 21.3 MB (42.49%) - LeetSync

--- Phase2/347-top-k-frequent-elements/top-k-frequent-elements.py
+++ Phase2/347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,31 +1,18 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        cnt = Counter(nums)
+
+        arr_cnt = []
+        for key,val in cnt.items():
+            arr_cnt.append(val)
+
+        top = nlargest(k,arr_cnt)
+
         res = []
-        count = Counter(nums)
-        # print(count)
-        for key,val in count.items():
-            res.append(val)
-
-        heapify(res)
-        ans = nlargest(k,res) 
-        # print(ans)
-        #count = count.most_common()
-        # count = sorted(count,key = count.get,reverse=True)
-        final_ = []
-        # for value in ans:
-        #     final_ = [key for key,val in count.items() if val == value]
-        for key,val in count.items():
-            if val in ans:
-                final_.append(key)
-
-        return final_
-        # print() 
-
-        # for i in range(k):
-            
-        #     res.append(count[])
+        for key,val in cnt.items():
+            if val in top:
+                res.append(key)
         
-        # return res 
-
+        return res
 
 
