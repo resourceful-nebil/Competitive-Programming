a896469a11f146795cebcfb51f9f40882a00a2bc
Nebiyou Elias <122722078+resourceful-nebil@users.noreply.github.com>
2024-09-19 09:42:07 +0300

    Time: 41 ms (55.41%), Space: 17.6 MB (6.95%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        def dfs(node,curSum):
+            if not node:
+                return False 
+            
+            curSum += node.val 
+            if not node.right and not node.left:
+                return curSum == targetSum 
+            
+            return dfs(node.right,curSum) or dfs(node.left,curSum)
+
+        return dfs(root,0)
